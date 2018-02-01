d190c5da5cab93efebba282b42156c6de474b23d
uyen <u.hoangth@gmail.com>
2018-02-01 13:57:15 -0500

    Add README.md and set up to install required packages automatically

---
+++ README.md
@@ -0,0 +1,3 @@
+# Consumer lifestyle segmentation
+
+This project was built in R language for the data team at Moutain Equipment Coorporation. Sharing outputs have been deidentified for privacy protection. 
--- settings.R
+++ settings.R
@@ -1,1 +1,6 @@
-data_path = 'data/'
+data_path <- 'data/'
+.packages <- c('dplyr', 'cluster', 'Rtsne', 'ggplot2', 'dplyr', 'tidyr', 'openxlsx', 'ggthemes', 'survey')
+
+# Install CRAN packages (if not already installed)
+.inst <- .packages %in% installed.packages()
+if(length(.packages[!.inst]) > 0) install.packages(.packages[!.inst])
