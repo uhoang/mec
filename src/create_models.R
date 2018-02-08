bfe91143f19af5665ad69c392791142fd007253f
uyen <u.hoangth@gmail.com>
2018-02-08 17:05:16 -0500

    update stability assessment for clusters by resampling

--- src/create_models.R
+++ src/create_models.R
@@ -57,72 +57,41 @@ ggsave(filename = 'viz/pam_silhouette_width_vs_num_clusters.png', width = 5.5, h
 #ggsave(filename = 'viz/kmean_silhouette_width_vs_num_clusters.png', width = 5.5, height = 3.5)
 
 
-# Divisive Hierarchical clustering
-hc <- diana(temp_train)
-sub_grp <- cutree(as.hclust(hc), k = 4)
-
-
-
-
-# kmean_fit <- kmeans(gower_dist, centers = 4)
-# fviz_cluster(list(data = temp_train, cluster = kmean_fit$cluster))
-
-
-# hc_a <- agnes(temp_train, method = 'complete')
-# sub_grp <- cutree(as.hclust(hc_a), k = 4)
-
-# fviz_nbclust(temp_train, FUN = hcut, method = 'silhouette')
-# fviz_cluster(list(data = temp_train, cluster = pam_fit$clustering))
-
-# clusGap(temp_train, FUN = hcut, nstart = 25, K.max = 10, B = 50)
-
-
-# pltree(hc, cex = .3, hang = -1, main = 'Dendrogram of diana')
-
-# hc5 <- hclust(gower_dist, method = 'ward.D2')
-
-# sub_grp <- cutree(hc5, k = 5)
-# table(sub_grp)
-
-# plot(hc5, cex = .3)
-# rect.hclust(hc5, k = 5, border = 2:6)
-# fviz_cluster(list(data = temp_train, cluster = sub_grp))
-
 # Fit data using PAM on number of clusters with the highest sihoutte width
 pam_fit <- pam(gower_dist, diss = TRUE, k = which.max(sil_width))
+pam_clusters <- pam_fit$clustering
 kmean_fit <- kmeans(gower_dist, centers = which.max(kmean_sil_width))
+kmean_clusters <- kmean_fit$cluster
 
-source('misc/make_tsne_plot.R')
 # Via visualization
 # Visualize many variables in a lower dimensional space with 
 # t-distributed stochastic neighborhood embedding (t-SNE) technique.
 # Allow us to view gower distance in 2D or 3D
+source('misc/make_tsne_plot.R')
 
 # Create t-SNE object from the obtained Gower distances
 tsne_obj <- Rtsne(gower_dist, is_distance = TRUE)
 
-# See how well-seperated clusters that PAM detect
-make_tsne_plot(tsne_obj, pam_fit$clustering, 
+# See how well-seperated clusters that PAM detects
+make_tsne_plot(tsne_obj, pam_clusters, 
               output = TRUE, 
               filepath = paste0('viz/pam_tsne_', which.max(sil_width), 'clusters.png'))
 
-# See how well-seperated clusters that PAM detect
-make_tsne_plot(tsne_obj, pam_fit$clustering,
+# See how well-seperated clusters that K-means detects
+make_tsne_plot(tsne_obj, kmean_clusters,
               output = TRUE,
               filepath = paste0('viz/kmean_tsne_', which.max(kmean_sil_width), 'clusters.png'))
 
 
-cluster[as.numeric(names(pam_fit$clustering))] <- paste0('Cluster', pam_fit$clustering)
+cluster[as.numeric(names(pam_clusters))] <- paste0('Cluster', pam_clusters)
 train$cluster <- cluster
 
 select_vars <- grep('h_|Q_|age_break|cluster|uuid|date', names(train), value = TRUE, ignore.case = TRUE)
-
 saveRDS(train[ , select_vars], 'output/clustered_data.rds')
-# saveRDS(train, 'output/train.rds')
 
-
-
-
+# Future work: try divisive hierarchical clustering (HC) with diana [in cluster package]
+# or agglomerative HC with agnes [in cluster package]
+# references: http://uc-r.github.io/hc_clustering
 
 
 # Cluster interpretation via descriptive statistics
@@ -132,120 +101,15 @@ pam_results <- train %>% dplyr::select(dplyr::one_of(c(vars, 'cluster'))) %>%
                          do(the_summary = summary(.))
 pam_results$the_summary
 
-
-
-# Via visualization 
-# plot gower distance for 6 clusters in 2D
-# gower_dist <- daisy(temp_train[ , vars], metric = 'gower')
-tsne_obj <- Rtsne(gower_dist, is_distance = TRUE)
-
-tsne_data <- tsne_obj$Y %>% data.frame() %>% 
-            setNames(c('X', 'Y')) %>% 
-            # mutate(tsncluster = factor(paste0('Cluster', pam_fit$clustering)))
-            mutate(tsncluster = factor(paste0('Cluster', kmean_fit$cluster)))
-            # mutate(tsncluster = factor(paste0('Cluster', sub_grp)))
-            # mutate(tsncluster = factor(train$cluster))
-
-names(tsne_data)[3] <- 'Group'
-ggplot(aes(x = X, y = Y), data = tsne_data) + 
-geom_point(aes(color = Group)) +
-my_theme()
-
-ggsave(filename = 'viz/tsne_cluster_6.png')
-
-# # plot gower distance for 7 cluster in 2D
-# gower_dist2 <- daisy(train[ , vars], metric = 'gower')
-# tsne_obj <- Rtsne(gower_dist2, is_distance = TRUE)
-
-# tsne_data <- tsne_obj$Y %>% data.frame() %>% 
-#             setNames(c('X', 'Y')) %>% 
-#             mutate(tsncluster = factor(paste0('Cluster', pam_fit$clustering)))
-#             # mutate(tsncluster = factor(train$cluster))
-
-# names(tsne_data)[3] <- 'Group'
-# ggplot(aes(x = X, y = Y), data = tsne_data) + 
-# geom_point(aes(color = Group)) +
-# my_theme()
-
-# ggsave(filename = 'viz/tsne_cluster_7.png')
-
-
+library(fpc)
 # Test stability of clusters using bootstrap
-# clusterboot(gower_dist, B = 10)
-# cf <- clusterboot(gower_dist, B = 100, 
-#                   bootmethod = c('boot'), 
-#                   clustermethod = claraCBI,
-#                   k = 6, seed = 12345)
+cf <- clusterboot(gower_dist, B = 100, 
+                  bootmethod = c('boot', 'subset', 'noise', 'jitter'), 
+                  clustermethod = claraCBI,
+                  k = 5, seed = 12345)
 
+print(cf, statistics = c('mean', 'dissolution', 'recovery'))
 
-# > cf$bootmean
-# [1] 0.8642140 0.7106713 0.6741667 0.7403177 0.7828143 0.7764324
-# the observations in cluster 1 have highly similar health condition and prevention,
-# distinct from those of the other cluster
+plot(cf, xlim=c(0,1),breaks=seq(0,1,by=0.05))
 
-# we can also say that the individuals in cluster 2-6 represent distinct health behaviours/patterns
-# but there isn't high certainty about which points should be clustered together.
-
-# number of times each cluster was dissolved. Clusters that are dissolved often are unstable. 
-# > cf$bootbrd
-# [1]  1 19 15 12  5  8
-
-
-#   Do the clustering. 
-# pfit <- hclust(gower_mat, method="ward")   
-
-#   Plot the dendrogram.
-# plot(pfit, labels=protein$Country)   
-
-
-
-
-# Building linear discriminant ananlysis for segments
-
-# library(caret)
-# library(MASS) # import library for LDA
-
-# train.lda <- train %>% dplyr::select(dplyr::one_of(vars)) %>%
-#                   mutate(cluster = factor(pam_fit$clustering))
-
-# train.lda2 <- train.lda
-# train.lda2$constant <- 1
-# scatterPlot(train.lda)
-
-# model <- lda(cluster ~ . , data = train.lda)
-
-# # create k-Fold cross-validation for LDA
-
-# folds <- 5
-
-# idx_list <- createFolds(train.lda$cluster, k = 4)
-
-# accuracy_list <- c()
-# for (i in 1:folds) {
-#   oos_idx <- idx_list[[i]]
-#   temp_train <- train.lda[-oos_idx, ]
-#   temp_test <- train.lda[oos_idx, ]
-#   model <- lda(cluster ~ . , data = temp_train)
-#   pred <- predict(model, temp_test)$class
-#   accuracy_list[i] <- mean(as.numeric(pred) == as.numeric(temp_test$cluster))
-
-# }
-
-# cat('The average of accuracy rate is:', mean(accuracy_list))
-
-
-
-# model <- lda(cluster ~ ., data = train.lda)
-
-# model_coef <- coef(model) # final product.
-
-# ntrain <- nrow(train.lda)
-
-# singlecov = lapply(1:4, function(x) {
-#   w <- (sum(train.lda$cluster == x) - 1)/(nrow(train.lda) - 4)
-#   covar <- w * cov(train.lda[train.lda$cluster == x, vars])
-#   return(covar)
-# })
-
-# singlecov = Reduce('+', singlecov)
 
